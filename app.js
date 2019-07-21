0325aed3016cc1b5003770795ab69a7f16f20a9d
Ricky Martinez <Ricky@rickys-imac.home>
2019-07-21 16:03:37 -0700

    Change List from arr to object & create form of display.

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const todos = ['item 1', 'item 2', 'item 3'];
+// const todos = ['item 1', 'item 2', 'item 3'];
+//
+// function displayTodos() {
+//   console.log(`My Todos: ${todos}`);
+// }
+//
+// function addTodo() {
+//   todos.push('new todo');
+// }
 
-function displayTodos() {
-  console.log(`My Todos: ${todos}`);
-}
-
-function addTodo() {
-  todos.push('new todo');
-}
+var todoList = {
+  todos: ['item 1', 'item 2', 'item 3'],
+  displayTodos: function() {
+    console.log('My Todos: ', this.todos);
+  }
+};
